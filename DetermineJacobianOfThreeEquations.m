8b8889b6abae10903210a3e7da795f09fd20ae71
Skaduvee <61471350+Skaduvee@users.noreply.github.com>
2023-04-17 19:22:50 +0200

    Ignore

--- DetermineJacobianOfThreeEquations.m
+++ DetermineJacobianOfThreeEquations.m
@@ -1,2 +1,3 @@
-function x = DetermineJacobianOfThreeEquations(F)
+function j = DetermineJacobianOfThreeEquations(F, h, point_one, point_two, point_three)
+j = (F(point_one + h, point_two + h, point_three + h) - F(point_one, point_two, point_three)) / h;
 end
--- DetermineJacobianOfTwoEquations.m
+++ DetermineJacobianOfTwoEquations.m
@@ -1,2 +1,3 @@
-function x = DetermineJacobianOfTwoEquations(F)
+function x = DetermineJacobianOfTwoEquations(F, h, point_one, point_two)
+x = (F(point_one + h, point_two + h) - F(point_one, point_two)) / h;
 end
